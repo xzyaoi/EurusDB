6c857bd2d638d55b523cb86de1aa248f262847b3
Xiaozhe Yao <askxzyao@gmail.com>
2021-05-21 03:40:49 +0200

    code format

--- Makefile
+++ Makefile
@@ -2,8 +2,4 @@ format:
 	autoflake --in-place --remove-unused-variables --recursive .
 	isort .
 	yapf -ir .
-
-docs:
-	python3 -m pdoc --html --output-dir docs src/indexing
-	mv docs/indexing/* docs/
-	rm -rf docs/indexing
+	cd src/engine && cargo fmt
--- src/engine/src/storage/kv/mod.rs
+++ src/engine/src/storage/kv/mod.rs
@@ -5,3 +5,17 @@
 
 use crate::utility::error::Result;
 use crate::sql::types;
+
+#[cfg(test)]
+pub use test::Test;
+
+use crate::error::Result;
+use std::fmt::Display;
+use std::ops::{Bound, RangeBounds};
+
+// A key/value store
+pub trait Store: Display + Send + Sync {
+    fn delete(&mut self, key: &[u8]) -> Result<()>;
+    fn flush(&mutself) -> Result<()>;
+    
+}
